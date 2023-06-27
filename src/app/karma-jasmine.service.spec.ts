7ba374c6a711a16c970be67b3443b250dbb23f78
PabloBeRui <pablo.bellon.ruibal@gmail.com>
2023-06-27 14:49:04 +0200

    Jasmine and Karma first contact

--- src/app/karma-jasmine.service.spec.ts
+++ src/app/karma-jasmine.service.spec.ts
@@ -1,7 +1,7 @@
-import { TestBed } from '@angular/core/testing';
-import { KarmaJasmineService } from './karma-jasmine.service';
+import { TestBed } from "@angular/core/testing";
+import { KarmaJasmineService } from "./karma-jasmine.service";
 
-describe('KarmaJasmineService', () => {
+describe("KarmaJasmineService", () => {
   let service: KarmaJasmineService;
 
   beforeEach(() => {
@@ -9,23 +9,60 @@ describe('KarmaJasmineService', () => {
     service = TestBed.get(KarmaJasmineService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 
-  it('debería sumar dos números correctamente', () => {
+  it("debería sumar dos números correctamente", () => {
     const resultado = service.sumar(2, 3);
     expect(resultado).toBe(5);
   });
 
-  it('debería sumar números negativos correctamente', () => {
+  it("debería sumar números negativos correctamente", () => {
     const resultado = service.sumar(-5, -3);
     expect(resultado).toBe(-8);
   });
 
-  it('debería sumar un número positivo y un número negativo correctamente', () => {
+  it("debería sumar un número positivo y un número negativo correctamente", () => {
     const resultado = service.sumar(10, -5);
     expect(resultado).toBe(5);
   });
 
+  it("debería restar dos números correctamente", () => {
+    const resultado = service.restar(2, 3);
+    expect(resultado).toBe(-1);
+  });
+
+  it("debería restar dos números correctamente", () => {
+    const resultado = service.restar(3, 2);
+    expect(resultado).toBe(1);
+  });
+  it("debería restar dos números correctamente", () => {
+    const resultado = service.restar(1, 1);
+    expect(resultado).toBe(0);
+  });
+  it("debería multiplicar dos números correctamente", () => {
+    const resultado = service.multiplicar(1, 1);
+    expect(resultado).toBe(1);
+  });
+  it("debería multiplicar dos números correctamente", () => {
+    const resultado = service.multiplicar(1, 0);
+    expect(resultado).toBe(0);
+  });
+  it("debería multiplicar dos números correctamente", () => {
+    const resultado = service.multiplicar(1, 12);
+    expect(resultado).toBe(12);
+  });
+  it("debería dividir dos números correctamente", () => {
+    const resultado = service.dividir(1, 1);
+    expect(resultado).toBe(1);
+  });
+  it("debería dividir dos números correctamente", () => {
+    const resultado = service.dividir(10, 1);
+    expect(resultado).toBe(10);
+  });
+  it("debería dividir dos números correctamente", () => {
+    const resultado = service.dividir(1, 0);
+    expect(resultado).toBe(Infinity);
+  });
 });
--- src/app/karma-jasmine.service.ts
+++ src/app/karma-jasmine.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class KarmaJasmineService {
-
-  constructor() { }
+  constructor() {}
 
   sumar(a: number, b: number): number {
     return a + b;
   }
 
+  restar(a: number, b: number): number {
+    return a - b;
+  }
 
+  multiplicar(a: number, b: number): number {
+    return a * b;
+  }
+
+  dividir(a: number, b: number): number {
+    return a / b;
+  }
 }
